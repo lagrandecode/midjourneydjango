cf1a811758fd20bdcb17dff95952565f3530051a
lagrandecode <seunogunmolufirst1@gmail.com>
2025-03-21 23:39:14 -0400

    "commit"

--- api/models.py
+++ api/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class Users(models.Model):
     name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     story = models.CharField(max_length=1000)
     image = models.ImageField(upload_to="images/")
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
 
     def __str__(self):
         return self.name
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from .models import User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 
 # Create your views here.
